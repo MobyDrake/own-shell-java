38a1e81bf1400040341f425155cf69e3534378bd
rostislav.bespalov <rostbespalov@gmail.com>
2025-11-27 17:00:41 +0500

    Implement exit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,16 @@
+private static final String PROMPT = "$ ";
+private static final String COMMAND_NOT_FOUND_MSG = "%s: command not found";
+private static final String EXIT_COMMAND = "exit";
+
 void main() {
-    IO.print("$ ");
-    String line = IO.readln();
-    IO.println(line + ": command not found");
+    boolean exit = false;
+    while (!exit) {
+        IO.print(PROMPT);
+        String line = IO.readln();
+        if (EXIT_COMMAND.equals(line)) {
+            exit = true;
+        } else {
+            IO.println(COMMAND_NOT_FOUND_MSG.formatted(line));
+        }
+    }
 }
